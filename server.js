db1cffcb18146b8f917cfe14290f55d4e52a677f
amal <k.amalelwan2001@gmail.com>
2025-05-11 00:31:39 +0300

    updates

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const userRoutes = require('./routes/userRoutes');
-const achievementsRoutes = require('./routes/achievementRoutes');
-const sequelize = require('./config/db');
+const userRoutes = require('./src/routes/userRoutes');
+const achievementsRoutes = require('./src/routes/achievementRoutes');
+const sequelize = require('./src/config/db');
 const cors = require("cors");
-const sectionRoutes = require("./routes/sectionRoutes");
-const quizRoutes = require('./routes/quizRoutes');
-const lessonRoutes = require("./routes/lessonRoutes");
+const sectionRoutes = require("./src/routes/sectionRoutes");
+const quizRoutes = require('./src/routes/quizRoutes');
+const lessonRoutes = require("./src/routes/lessonRoutes");
 const bodyParser = require('body-parser');
 const path = require("path");
-const chatbotRoutes = require('./routes/chatbotRoutes');
+const chatbotRoutes = require('./src/routes/chatbotRoutes');
 const cookieParser = require("cookie-parser");
-const models = require('./models/index');
-const avatarRoutes = require('./routes/avatarRoutes');
-const motivationRoutes = require('./routes/motivationRoutes');
-const feedbackRoutes = require('./routes/feedbackRoutes');
-const analyticsEventRoutes = require('./routes/analyticsEventRoutes');
+const models = require('./src/models/index');
+const avatarRoutes = require('./src/routes/avatarRoutes');
+const motivationRoutes = require('./src/routes/motivationRoutes');
+const feedbackRoutes = require('./src/routes/feedbackRoutes');
+const analyticsEventRoutes = require('./src/routes/analyticsEventRoutes');
 
 dotenv.config();
 
@@ -26,8 +26,6 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-
-
 
 const corsOptions = {
   origin: 'http://localhost:3000',
@@ -50,8 +48,8 @@ app.use("/sections", sectionRoutes);
 app.use('/api/quiz', quizRoutes);
 app.use("/api", lessonRoutes);
 app.use('/api/chatbot', chatbotRoutes);
-app.use("/videos", express.static(path.join(__dirname, "public/videos")));
-app.use("/images", express.static(path.join(__dirname, "public/images")));
+// app.use("/videos", express.static(path.join(__dirname, "public/videos"))); // Removed
+// app.use("/images", express.static(path.join(__dirname, "public/images"))); // Removed
 app.use('/', avatarRoutes);
 app.use('/api', motivationRoutes);
 app.use('/api/feedback', feedbackRoutes);
