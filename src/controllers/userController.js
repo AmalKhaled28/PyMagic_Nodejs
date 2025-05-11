2d3a8443fa0f462154d96e5f919237659dcc7443
amal <k.amalelwan2001@gmail.com>
2025-05-11 19:01:34 +0300

    updates

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -124,20 +124,20 @@ const loginUser = async (req, res) => {
       expiresIn: rememberMe ? '7d' : '1h',
     });
 
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // لازم يكون true على Railway لأنه HTTPS
-      sameSite: 'None', // عشان يسمح بتبادل الكوكيز عبر domains مختلفة
-    });
-
-    // //
-    // res.cookie("token", token, {
-    //   maxAge: rememberMe ? 7 * 24 * 60 * 60 * 1000 : 60 * 60 * 1000,
-    //   httpOnly: true,     // Recommended for security
-    //   secure: true,       // HTTPS-only
-    //   sameSite: "None",   // Allow cross-origin
+    // res.cookie('token', token, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === 'production', // لازم يكون true على Railway لأنه HTTPS
+    //   sameSite: 'None', // عشان يسمح بتبادل الكوكيز عبر domains مختلفة
     // });
+
     //
+    res.cookie("token", token, {
+      maxAge: rememberMe ? 7 * 24 * 60 * 60 * 1000 : 60 * 60 * 1000,
+      httpOnly: true,     // Recommended for security
+      secure: true,       // HTTPS-only
+      sameSite: "None",   // Allow cross-origin
+    });
+    
     
     let lastSectionId = 1;
     const mostRecentQuiz = await StudentQuiz.findOne({
